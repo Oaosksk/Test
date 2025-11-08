0325f3f2b6d1ef4d8f7c4708c85f2f790b5cf0bf
mvignesh <vignesh.m@macincode.com>
2025-11-08 11:14:41 +0530

    Check

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Module 1 button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const buttonElement = screen.getByText(/module 1/i);
+  expect(buttonElement).toBeInTheDocument();
 });
